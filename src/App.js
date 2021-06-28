70e6c92f76f56440b86958cc7a36f0257c1782b0
Aman <amanchopra.atg@gmail.com>
2021-06-28 18:08:17 +0530

    task displaying feature updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const App = () => {
+
+  const [inputList, setInputList] = useState("");
+  const [Items, setItems] = useState([]);
+
+  const itemEvent = (event) => {
+    setInputList(event.target.value);
+  };
+
+  const listOfItems = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList]
+    })
+    setInputList("");
+  }
+
   return (
-    <div>
-      <h1> To Do App </h1>
+    <div className="main_div">
+      <div className="center_div">
+        <br />
+        <h1>My Personal To Do List</h1>
+        <br />
+        <input type="text" placeholder="Add your task" value={inputList} onChange={itemEvent} />
+        <button onClick={listOfItems}> + </button>
+
+        <ol>
+
+          {Items.map( (itemval) => {
+            return <li> {itemval} </li>
+          })}
+
+        </ol>
+
+      </div>
     </div>
   )
 }
